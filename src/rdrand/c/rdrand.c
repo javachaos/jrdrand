dd99eb48279ad3cef1bc908b2d6f18daf377915f
Javachaos <alfred_e@outlook.com>
2022-07-29 19:49:45 -0400

    Removed whitespace and comments in rdrand.c

--- src/rdrand/c/rdrand.c
+++ src/rdrand/c/rdrand.c
@@ -3,32 +3,29 @@
 #include <jni.h>
 
 #if defined(__x86_64__) || defined (_M_X64)
-// Figure out which 64-bit type the output arg uses
-#ifdef __INTEL_COMPILER       // Intel declares the output arg type differently from everyone(?) else
-// ICC for Linux declares rdrand's output as unsigned long, but must be long long for a Windows ABI
+#ifdef __INTEL_COMPILER
 typedef uint64_t intrin_u64;
 #else
-// GCC/clang headers declare it as unsigned long long even for Linux where long is 64-bit,
-// but uint64_t is unsigned long and not compatible
 typedef unsigned long long intrin_u64;
 #endif
-
-#if defined(__RDRND__) || defined(_MSC_VER)  // conditional definition if you want
-uint64_t rdrand64(){
-    intrin_u64 ret;
-    do{}while( !_rdrand64_step(&ret) );  // retry until success.
-    return ret;
+#if defined(__RDRND__) || defined(_MSC_VER)
+uint64_t rdrand64()
+{
+    intrin_u64 r;
+    do {} while( !_rdrand64_step(&r) );
+    return r;
 }
 #endif
 
 #if defined(__RDSEED__) || defined(_MSC_VER)
-uint64_t rdseed64() {
-    intrin_u64 ret;
-    do{}while( !_rdseed64_step(&ret) );   // retry until success.
-    return ret;
+uint64_t rdseed64()
+{
+    intrin_u64 r;
+    do {} while( !_rdseed64_step(&r) );
+    return r;
 }
-#endif  // RDSEED
-#endif  // x86-64
+#endif
+#endif
 
 JNI_OnLoad_rdrand(JavaVM *vm, void *reserved)
 {

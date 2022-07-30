285ee18d779d458dc980c2748b538b19d875a35b
fred <alfred_e@outlook.com>
2022-07-29 22:53:15 -0400

    added pause instruction to rdrand.c in the spin loop for rdrand and rdseed functions.

--- src/rdrand/c/rdrand.c
+++ src/rdrand/c/rdrand.c
@@ -1,6 +1,10 @@
-#include <immintrin.h>
 #include <stdint.h>
 #include <jni.h>
+#ifdef __GNUC__
+#include <x86intrin.h>
+#elif __MSC_VER__
+#include <immintrin.h>
+#endif
 
 #if defined(__x86_64__) || defined (_M_X64)
 #ifdef __INTEL_COMPILER
@@ -11,19 +15,19 @@ typedef unsigned long long intrin_u64;
 uint64_t rdrand64()
 {
     intrin_u64 r;
-    do {} while( !_rdrand64_step(&r) );
+    do { _mm_pause(); } while( !_rdrand64_step(&r) );
     return r;
 }
 
 uint64_t rdseed64()
 {
     intrin_u64 r;
-    do {} while( !_rdseed64_step(&r) );
+    do { _mm_pause(); } while( !_rdseed64_step(&r) );
     return r;
 }
 #endif
 
-JNI_OnLoad_rdrand(JavaVM *vm, void *reserved)
+void JNI_OnLoad_rdrand(JavaVM *vm, void *reserved)
 {
     return JNI_VERSION_1_8;
 }
